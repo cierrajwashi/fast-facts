78ee93e44771cd2f05938bb868868687628d3338
Cierra <cierrajwashi@gmail.com>
2020-09-27 13:24:27 -0400

    final heroku deployment debugging

--- server/models/statement.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const StatementSchema = new mongoose.Schema({
-  statement: String,
-  answer: String,
-});
-
-module.exports = mongoose.model("Statements", StatementSchema);
