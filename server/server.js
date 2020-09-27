e6212898cd91ef961a566103e19c4a7b68acc0f8
Cierra <cierrajwashi@gmail.com>
2020-09-27 02:31:21 -0400

    heroku deployment

--- server/server.js
+++ server/server.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const StatementRoute = require('./routes/statementRoute')
+const StatementRoute = require("./routes/statementRoute");
 const app = express();
-const cors = require('cors')
+const cors = require("cors");
 
 express.json();
 app.use(bodyParser.json());
 
-app.use(cors())
+app.use(cors());
 
-mongoose.connect('mongodb://localhost/statement')
-.then(() => console.log("mongodb connected successfully"))
-.catch(() => console.log("err"))
+mongoose
+  .connect("mongodb://localhost/statement")
+  .then(() => console.log("mongodb connected successfully"))
+  .catch(() => console.log("err"));
 
-app.use('/', StatementRoute)
+app.use("/", StatementRoute);
 
 const port = process.env.port || 5000;
 
